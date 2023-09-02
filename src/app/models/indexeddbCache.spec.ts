27281c8797edfdb57592fc76326edd6052fc6b90
zhen <zhen.yang@hotmail.fr>
2023-09-02 16:38:38 +0200

    [feature/indexeddb] remove localstorage cache

---
+++ src/app/models/indexeddbCache.spec.ts
@@ -0,0 +1,7 @@
+import { IndexeddbCache } from './indexeddbCache';
+
+describe('indexeddbCache', () => {
+  it('should create an instance', () => {
+    expect(new IndexeddbCache()).toBeTruthy();
+  });
+});
