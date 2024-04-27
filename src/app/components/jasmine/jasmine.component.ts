c5a8038ed00079c40fbe825bce30a8dd263148eb
zhen <zhen.yang@hotmail.fr>
2024-04-27 17:16:32 +0200

    maj

--- src/app/components/jasmine/jasmine.component.ts
+++ src/app/components/jasmine/jasmine.component.ts
@@ -12,6 +12,7 @@ export class JasmineComponent implements OnInit {
   allPhotos: {url:string}[] = [];
   allVideos: {url:string}[] = [];
   section:number = 2;
+  photoIndex:number|null = null;
   public masonryOptions: NgxMasonryOptions = {
 		resize: true,
 		initLayout: true
@@ -38,5 +39,34 @@ export class JasmineComponent implements OnInit {
   switchSection(): void {
     if (this.section === 1)this.section = 2;
     else this.section = 1;
+  }
+
+  wheelVideos(e:WheelEvent): void {
+    if (this.section === 2) {
+      let div = e.target as HTMLDivElement;
+      let id:number = parseInt(div.id.split('-')[1]);
+      this.stopVideo(document.getElementById(`video-${id}`));
+      this.stopVideo(document.getElementById(`video-${id + 1}`));
+      this.setVideosUrl(id);
+    }
+  }
+  scrollVideos(e:Event): void {
+    if (this.section === 2) {
+      const video:HTMLVideoElement = document.getElementById('video-0') as HTMLVideoElement;
+      let id:number = Math.floor((e.target as HTMLDivElement).scrollTop / video.clientHeight);
+      this.stopVideo(document.getElementById(`video-${id - 1}`));
+      this.stopVideo(document.getElementById(`video-${id + 1}`));
+      this.setVideosUrl(id);
+    }
+  }
+
+  private stopVideo = (video:HTMLElement|null): void => {
+    (video as HTMLVideoElement)?.pause();
+  }
+
+  private setVideosUrl(index:number): void {
+    document.getElementById(`video-${index - 1}`)?.setAttribute('src', this.allVideos[index - 1]['url']);
+    document.getElementById(`video-${index}`)?.setAttribute('src', this.allVideos[index]['url']);
+    document.getElementById(`video-${index + 1}`)?.setAttribute('src', this.allVideos[index + 1]['url']);
   }
 }
